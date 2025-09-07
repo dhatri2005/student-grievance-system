32cbfda41fc75beebe43b5af48a1946effd583fe
dhatri2005 <156169899+dhatri2005@users.noreply.github.com>
2025-09-07 11:03:17 +0530

    Update addofficer.php

--- student grievance system/admin/addofficer.php
+++ student grievance system/admin/addofficer.php
@@ -1,20 +1,21 @@
 <?php
-    session_start();
+session_start();
 ?>
 
 <!DOCTYPE HTML>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Home</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    </head>
-    <body>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-        <?php include "../inc/header.php" ?>
-        <h2 class="py-3 px-5 text-center">Register Officer!</h2>
-        <div class="container px-5 my-5">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register Officer</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    <h2 class="py-3 px-5 text-center">Register Officer!</h2>
+
+    <div class="container px-5 my-5">
         <form id="contactForm" action="addofficerdata.php" method="POST">
             <div class="form-floating mb-3">
                 <input class="form-control" name="name" type="text" placeholder="Name" required />
@@ -41,5 +42,5 @@
             </div>
         </form>
     </div>
-    </body>
+</body>
 </html>
